ffe3468aa81586c01c8b623b2efef986568146d0
Cuong Ly <cuonglyy@gmail.com>
2023-01-30 16:27:59 -0800

    Adding JS form validation

---
+++ app.js
@@ -0,0 +1,13 @@
+const signUpBtn = document.querySelector('.sign-up__btn--submit');
+
+
+const signUp = (e) => {
+    e.preventDefault();
+
+    console.log('hello');
+}
+
+
+
+
+signUpBtn.addEventListener('click', signUp);
