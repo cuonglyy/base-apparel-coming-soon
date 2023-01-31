da9eec8f08b8483aea7a43841f7ba64697534a3a
Cuong Ly <cuonglyy@gmail.com>
2023-01-30 21:22:27 -0800

    Added form validation with JS

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+const form = document.querySelector('.sign-up');
 const signUpBtn = document.querySelector('.sign-up__btn--submit');
-
+const errorMsg = document.querySelector('.error-msg');
+const errorIcon = document.querySelector('.error-icon');
+const email = document.querySelector('.sign-up__field');
 
 const signUp = (e) => {
     e.preventDefault();
 
-    console.log('hello');
+    if (email.validity.typeMismatch || email.validity.valueMissing) {
+        form.classList.add('error');
+        errorMsg.style.opacity = '1';
+        errorIcon.style.opacity = '1';
+    } else {
+        form.classList.remove('error');
+        errorMsg.style.opacity = '0';
+        errorIcon.style.opacity = '0';
+    }
 }
 
 
